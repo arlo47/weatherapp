91793a2bc459fd0a457aeae0a39c50f8a0770be2
Greg Fitzpatrick <gfitzpatrck2@gmail.com>
2019-08-27 14:04:01 -0400

    some dom manipulation added

--- js/app.js
+++ js/app.js
@@ -9,16 +9,26 @@
   * add methods for 5 day forecast
   * add methods for 16 day forecast
   * figure out a way to specify weather by city name if search btn is clicked
+  * something is wrong with the order the view methods are running. view.displayCurrentWeather() is running before weatherData is populated
   */
 
 let weatherData = null;
+let date = new Date();
+
+let formatDate = () => {
+    let day = date.getDay();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
+    return day + "/" + month + "/" + year;
+}
 
 let apiHandler = {
     coords: null,
     cityName: document.getElementById("city-name-input"),
-    currentWeather: () => {
+    getCurrentWeather: () => {
         let locationURL = apiHandler.coords ? apiHandler.coords : "q=" + apiHandler.cityName.value;
-        fetch("https://api.openweathermap.org/data/2.5/weather?" + locationURL + "&APPID=f20349ac0af5ebfbc2586d5a8ae52834")
+        fetch("https://api.openweathermap.org/data/2.5/weather?" + locationURL + "&units=metric&APPID=f20349ac0af5ebfbc2586d5a8ae52834")
             .then(
                 (res) => {
                     if(res.status == 200) {
@@ -40,7 +50,7 @@ let apiHandler = {
         if(navigator.geolocation) {
             navigator.geolocation.getCurrentPosition((pos) => {
                 apiHandler.coords = "lat=" + pos.coords.latitude + "&lon=" + pos.coords.longitude;
-                apiHandler.currentWeather();
+                apiHandler.getCurrentWeather();
             });
         }
         else {
@@ -51,10 +61,32 @@ let apiHandler = {
 }
 
 let view = {
-    output: document.getElementById("output"),
-    currentWeather: () => {
-        output.innerHTML = weatherData.weather[0].description;
+    displayCurrentWeather: () => {
+        let currentTempOutput = document.getElementById("current-temp");
+        let currentHighTempOutput = document.getElementById("current-high-temp");
+        let currentLowTempOutput = document.getElementById("current-low-temp");
+
+        currentTempOutput.innerHTML = weatherData.main.temp;
+        currentHighTempOutput.innerHTML = weatherData.main.temp_max;
+        currentLowTempOutput.innerHTML = weatherData.main.temp_min;
+    },
+    displayCurrentDay: () => {
+        let currentDayOutput = document.getElementById("current-day");
+        let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        let dayName = days[date.getDay()];
+                
+        currentDayOutput.innerHTML = dayName;
+    },
+    displayCurrentDate: () => {
+        let currentDateOutput = document.getElementById("current-date");
+
+        currentDateOutput.innerHTML = formatDate();
     }
 }
 
-window.addEventListener("load", apiHandler.getLocation);
+
+apiHandler.getLocation();
+
+window.addEventListener("load", view.displayCurrentDate);
+window.addEventListener("load", view.displayCurrentDay);
+window.addEventListener("load", view.displayCurrentWeather);
