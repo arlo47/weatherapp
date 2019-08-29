66a97c5c4130d81f0357c9c30ae7c8087518d66c
Greg Fitzpatrick <gfitzpatrck2@gmail.com>
2019-08-29 10:30:30 -0400

    theeHourForecast API working

--- js/app.js
+++ js/app.js
@@ -9,10 +9,11 @@
   * add methods for 5 day forecast
   * add methods for 16 day forecast
   * figure out a way to specify weather by city name if search btn is clicked
-  * something is wrong with the order the view methods are running. view.displayCurrentWeather() is running before weatherData is populated
   */
 
 let weatherData = null;
+let threeHourForecast = null;
+let dailyForecast = null;
 let date = new Date();
 
 let formatDate = () => {
@@ -26,26 +27,6 @@ let formatDate = () => {
 let apiHandler = {
     coords: null,
     cityName: document.getElementById("city-name-input"),
-    getCurrentWeather: () => {
-        let locationURL = apiHandler.coords ? apiHandler.coords : "q=" + apiHandler.cityName.value;
-        fetch("https://api.openweathermap.org/data/2.5/weather?" + locationURL + "&units=metric&APPID=f20349ac0af5ebfbc2586d5a8ae52834")
-            .then(
-                (res) => {
-                    if(res.status == 200) {
-                        console.log("Status is " + res.status);
-                        res.json().then((data) => {
-                            weatherData = data;
-                        });
-                    }
-                    else {
-                        console.log("Error. Status is " + res.status);
-                    }
-                }
-            )
-            .catch((err) => {
-                console.log("Fetch Error: " + err);
-            });
-    },
     getLocation: () => {
         if(navigator.geolocation) {
             navigator.geolocation.getCurrentPosition((pos) => {
@@ -57,6 +38,55 @@ let apiHandler = {
             console.log("Geolocation is not supported by your browser. Please enter your city name manually.");
             return;
         }
+    },
+    getCurrentWeather: () => {
+        let locationURL = apiHandler.coords ? apiHandler.coords : "q=" + apiHandler.cityName.value;
+
+        fetch("https://api.openweathermap.org/data/2.5/weather?" + locationURL + "&units=metric&APPID=f20349ac0af5ebfbc2586d5a8ae52834")
+            .then(
+                (res) => {
+                    if(res.status == 200) {
+                        console.log("Status is " + res.status);
+                        res.json().then((data) => {
+                            //Once data is assigned to weatherData, display content to DOM
+                            weatherData = data;
+                            view.displayCurrentDate();
+                            view.displayCurrentDay();
+                            view.displayCurrentWeather();
+                        });
+                    }
+                    else {
+                        console.log("Error. Status is " + res.status);
+                    }
+                }
+            )
+            .catch((err) => {
+                console.log("Fetch Error: " + err);
+            });
+    },
+    getThreeHourForecast: () => {
+        //By geo location: api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}
+        //By city name: api.openweathermap.org/data/2.5/forecast?q={city name},{country code}
+        let locationURL = apiHandler.coords ? apiHandler.coords : "q=" + apiHandler.cityName.value;
+
+        fetch("https://api.openweathermap.org/data/2.5/forecast?" + locationURL + "&units=metric&APPID=f20349ac0af5ebfbc2586d5a8ae52834")
+            .then(
+                (res) => {
+                    if(res.status == 200) {
+                        console.log("status is " + res.status);
+                        res.json().then((data) => {
+                            threeHourForecast = data;
+                            console.log(threeHourForecast);
+                        });
+                    }
+                    else {
+                        console.log("Error. Status is " + res.status);
+                    }
+                }
+            )
+            .catch((err) => {
+                console.log("Fetch Error: " + err);
+            });
     }
 }
 
@@ -84,9 +114,4 @@ let view = {
     }
 }
 
-
-apiHandler.getLocation();
-
-window.addEventListener("load", view.displayCurrentDate);
-window.addEventListener("load", view.displayCurrentDay);
-window.addEventListener("load", view.displayCurrentWeather);
+window.addEventListener("load", apiHandler.getLocation);
