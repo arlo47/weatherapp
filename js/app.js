2bb9a735b8a69ba0a2386770fdc1ffb5a6660552
Greg Fitzpatrick <gfitzpatrck2@gmail.com>
2019-09-01 18:22:53 -0400

    svg icons added, app.js restructured

--- js/app.js
+++ js/app.js
@@ -14,13 +14,14 @@
  * 50d    mist                701
  */
 
-let weatherData = null;
-let threeHourForecast = null;
-let dailyForecast = null;
+let weatherData = null;     //contains all weather data pulled from OpenWeather API
+let forecastData = null;    //contains all forecast data pulled from OpenWeather API
+let coords = null;          //contains latitude and longitude values from getLocation()
+let cityName = null;        //contains city name value when user enters city name
+let date = new Date();
 
+//returns date in dd/mm/yyyy format
 let formatDate = () => {
-    let date = new Date();
-
     let day = date.getDay();
     let month = date.getMonth() + 1;
     let year = date.getFullYear();
@@ -28,84 +29,70 @@ let formatDate = () => {
     return day + "/" + month + "/" + year;
 }
 
-let apiHandler = {
-    coords: null,
-    cityName: document.getElementById("city-name-input"),
-    getLocation: () => {
-        if(navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition((pos) => {
-                apiHandler.coords = "lat=" + pos.coords.latitude + "&lon=" + pos.coords.longitude;
-                apiHandler.getCurrentWeather();
-                apiHandler.getThreeHourForecast();
-            });
-        }
-        else {
-            console.log("Geolocation is not supported by your browser. Please enter your city name manually.");
-            return;
-        }
-    },
-    //the URL is the same for both fetch methds, they can be combined
-    //modify locationURL by geolocation or city name depending on parameters given
-    getCurrentWeather: () => {
-        let locationURL = apiHandler.coords ? apiHandler.coords : "q=" + apiHandler.cityName.value;
+//uses the browsers geolocation method to get users coordinates
+let getLocation = () => {
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((pos) => {
+            coords = "lat=" + pos.coords.latitude + "&lon=" + pos.coords.longitude;
+            fetchFromAPI("weather");
+            fetchFromAPI("forecast");
+        });
+    }
+    else {
+        console.log("Geolocation is not supported by your browser. Please enter your city name manually.");
+        return;
+    }
+}
 
-        fetch("https://api.openweathermap.org/data/2.5/weather?" + locationURL + "&units=metric&APPID=f20349ac0af5ebfbc2586d5a8ae52834")
-            .then(
-                (res) => {
-                    if(res.status == 200) {
-                        console.log("Status is " + res.status);
-                        res.json().then((data) => {
-                            //Once data is assigned to weatherData, display content to DOM
+//type can be either weather or forecast, weather returns current weather info, forecast returns 5 day/3 hour forecast
+let fetchFromAPI = (type) => {
+    let locationURL = coords ? coords : "q=" + cityName.value;
+
+    fetch("https://api.openweathermap.org/data/2.5/" + type + "?" + locationURL + "&units=metric&APPID=f20349ac0af5ebfbc2586d5a8ae52834")
+        .then(
+            (res) => {
+                if(res.status == 200) {
+                    console.log("Status is " + res.status);
+                    res.json().then((data) => {
+                        //Once data is assigned to weatherData, display content to DOM
+                        if(type == "weather") {
                             weatherData = data;
                             view.displayCurrentDate();
                             view.displayCurrentDay();
                             view.displayCurrentWeather();
-                        });
-                    }
-                    else {
-                        console.log("Error. Status is " + res.status);
-                    }
-                }
-            )
-            .catch((err) => {
-                console.log("Fetch Error: " + err);
-            });
-    },
-    getThreeHourForecast: () => {
-        //By geo location: api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}
-        //By city name: api.openweathermap.org/data/2.5/forecast?q={city name},{country code}
-        let locationURL = apiHandler.coords ? apiHandler.coords : "q=" + apiHandler.cityName.value;
-
-        fetch("https://api.openweathermap.org/data/2.5/forecast?" + locationURL + "&units=metric&APPID=f20349ac0af5ebfbc2586d5a8ae52834")
-            .then(
-                (res) => {
-                    if(res.status == 200) {
-                        console.log("status is " + res.status);
-                        res.json().then((data) => {
-                            threeHourForecast = data;
+                        }
+                        else if(type == "forecast") {
+                            forecastData = data;
                             view.displayThreeHourForecast();
-                        });
-                    }
-                    else {
-                        console.log("Error. Status is " + res.status);
-                    }
+                        }
+                        else {
+                            console.log("type was not weather or forecast");
+                        }
+                    });
                 }
-            )
-            .catch((err) => {
-                console.log("Fetch Error: " + err);
-            });
-    }
+                else {
+                    console.log("Error. Status is " + res.status);
+                }
+            }
+        )
+        .catch((err) => {
+            console.log("Fetch Error: " + err);
+        });
 }
+
 
 let view = {
     displayCurrentWeather: () => {
         let currentTempOutput = document.getElementById("current-temp");
         let currentHighTempOutput = document.getElementById("current-high-temp");
         let currentLowTempOutput = document.getElementById("current-low-temp");
+        let currentWeatherIconOutput = document.getElementById("current-weather-icon");
 
         currentTempOutput.innerHTML = weatherData.main.temp;
         currentHighTempOutput.innerHTML = weatherData.main.temp_max;
         currentLowTempOutput.innerHTML = weatherData.main.temp_min;
+        //gets icon code from weatherData and inserts it to the src of an <img> with displayIcon()
+        currentWeatherIconOutput.innerHTML = view.displayIcon(weatherData.weather[0].icon);
     },
     displayCurrentDay: () => {
         let currentDayOutput = document.getElementById("current-day");
@@ -120,18 +107,27 @@ let view = {
         currentDateOutput.innerHTML = formatDate();
     },
     displayThreeHourForecast: () => {
-        let threeHourForecastOutputList = document.querySelectorAll("div.three-hour-forecast > div");
-        console.log(threeHourForecastOutputList);
+        let forecastDataOutputList = document.querySelectorAll("div.three-hour-forecast > div.col-12 > div.row");
+        console.log(forecastDataOutputList);
 
-        for(let i = 0; i < threeHourForecastOutputList.length; i++) {
-            threeHourForecastOutputList[i].innerHTML =  "<div>" +
-                                                            "<span>" + threeHourForecast.list[i].dt_txt + "</span>" +
-                                                            "<span>" + threeHourForecast.list[i].main.temp + "</span>" +
-                                                        "</div>" +
-                                                        "<span>" + threeHourForecast.list[i].main.temp_max + "</span>"
-                                                        "<span>" + threeHourForecast.list[i].main.temp_min + "</span>";
+        for(let i = 0; i < forecastDataOutputList.length; i++) {
+            forecastDataOutputList[i].innerHTML =   "<div class='col-5'>" + forecastData.list[i].dt_txt + "</div>" +
+                                                    //throwing an error
+                                                    "<div class='col-1'>" + "<img class='icon' src='img/icons/11d.svg'>" + "</div>" +
+                                                    "<div class='col-3'>" + forecastData.list[i].weather[0].description + "</div>" +
+                                                    "<div class='col-1'>" + forecastData.list[i].main.temp + "</div>" +
+                                                    "<div class='col-1'>" + forecastData.list[i].main.temp_max + "</div>" +
+                                                    "<div class='col-1'>" + forecastData.list[i].main.temp_min + "</div>";
         }
+    },
+    displayIcon: (iconCode) => {
+        return "<img class='icon' src='img/icons/" + iconCode + ".svg'>";
     }
 }
 
-window.addEventListener("load", apiHandler.getLocation);
+
+
+
+
+
+window.addEventListener("load", getLocation);
